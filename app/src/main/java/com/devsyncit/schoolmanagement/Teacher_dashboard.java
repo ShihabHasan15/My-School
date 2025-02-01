6233568f76be685004253b567af0cf6284cc83d7
Shihab <shihabhasanhridoy06@gmail.com>
2025-02-02 00:05:37 +0600

    teacher_backend_improvement

--- app/src/main/java/com/devsyncit/schoolmanagement/Teacher_dashboard.java
+++ app/src/main/java/com/devsyncit/schoolmanagement/Teacher_dashboard.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -29,6 +30,8 @@ public class Teacher_dashboard extends AppCompatActivity {
     ImageSlider imageSlider;
     List<SlideModel> teacher_notice_board;
     MaterialCardView view_students_btn;
+    TextView teacher_profile_name, teacher_profile_email, students_count_under_teacher;
+    public static int total_students = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +41,32 @@ public class Teacher_dashboard extends AppCompatActivity {
         timeLineRecyclerView = findViewById(R.id.timeLineRecyclerView);
         imageSlider = findViewById(R.id.teacher_dash_image_slider);
         view_students_btn = findViewById(R.id.view_students_btn);
+        teacher_profile_name = findViewById(R.id.teacher_profile_name);
+        teacher_profile_email = findViewById(R.id.teacher_profile_email);
+        students_count_under_teacher = findViewById(R.id.students_count_under_teacher);
+
+            Intent intent = getIntent();
+            String t_name = intent.getStringExtra("teacher_name");
+            String t_email = intent.getStringExtra("teacher_email");
+            String t_phone_number = intent.getStringExtra("teacher_phone_number");
+            String t_id = intent.getStringExtra("teacher_id");
+
+
+            teacher_profile_name.setText(t_name);
+            teacher_profile_email.setText(t_email);
+            students_count_under_teacher.setText(""+total_students);
 
         view_students_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Teacher_dashboard.this, teacher_classes_name.class));
+                Intent postIntent = new Intent(Teacher_dashboard.this, teacher_classes_name.class);
+
+                postIntent.putExtra("teacher_name", ""+t_name);
+                postIntent.putExtra("teacher_email", ""+t_email);
+                postIntent.putExtra("teacher_mobile_number", ""+t_phone_number);
+                postIntent.putExtra("teacher_id", ""+t_id);
+
+                startActivity(postIntent);
             }
         });
 
--- app/src/main/java/com/devsyncit/schoolmanagement/teacher_classes_name.java
+++ app/src/main/java/com/devsyncit/schoolmanagement/teacher_classes_name.java
@@ -1,9 +1,12 @@
 package com.devsyncit.schoolmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -14,22 +17,91 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class teacher_classes_name extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class teacher_classes_name extends AppCompatActivity {
     RecyclerView classes_and_section;
+    HashMap<String, String> hashMap;
+    ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_classes_name);
 
-
         classes_and_section = findViewById(R.id.classes_and_section);
 
-        ClassesAndSectionAdapter adapter = new ClassesAndSectionAdapter();
-        classes_and_section.setAdapter(adapter);
+        Intent intent = getIntent();
 
-        classes_and_section.setLayoutManager(new LinearLayoutManager(teacher_classes_name.this));
+        int t_id = Integer.parseInt(intent.getStringExtra("teacher_id"));
+
+        RequestQueue queue = Volley.newRequestQueue(teacher_classes_name.this);
+        String url = "http://192.168.0.108/Apps/teacher_course_list_data_get.php?t_id="+t_id;
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url
+                , null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                arrayList.clear();
+
+                int sum_of_students = 0;
+
+                for (int i=0; i<response.length(); i++){
+
+                    try {
+
+                        JSONObject jsonObject = response.getJSONObject(i);
+
+                        hashMap = new HashMap<>();
+                        hashMap.put("name", jsonObject.getString("name"));
+                        hashMap.put("Class", jsonObject.getString("class"));
+                        hashMap.put("section", jsonObject.getString("section"));
+                        String no_of_students = jsonObject.getString("no_of_students");
+                        hashMap.put("no_of_students", no_of_students);
+                        hashMap.put("course_name", jsonObject.getString("course_name"));
+
+                        arrayList.add(hashMap);
+
+                        sum_of_students = sum_of_students + Integer.parseInt(no_of_students);
+
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }
+
+                Teacher_dashboard.total_students = sum_of_students;
+
+                ClassesAndSectionAdapter adapter = new ClassesAndSectionAdapter();
+                classes_and_section.setAdapter(adapter);
+
+                classes_and_section.setLayoutManager(new LinearLayoutManager(teacher_classes_name.this));
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(jsonArrayRequest);
+
+
 
     }
 
@@ -37,8 +109,15 @@ public class teacher_classes_name extends AppCompatActivity {
 
         public class ClassesAndSectionViewHolder extends RecyclerView.ViewHolder{
 
+            TextView class_no_and_section_name, no_of_students, course_name;
+
             public ClassesAndSectionViewHolder(@NonNull View itemView) {
                 super(itemView);
+
+                class_no_and_section_name = itemView.findViewById(R.id.class_no_section_name);
+                no_of_students = itemView.findViewById(R.id.no_of_students);
+                course_name = itemView.findViewById(R.id.course_name);
+
             }
         }
 
@@ -55,11 +134,24 @@ public class teacher_classes_name extends AppCompatActivity {
         @Override
         public void onBindViewHolder(@NonNull ClassesAndSectionViewHolder holder, int position) {
 
+                HashMap<String, String> get_course_data = arrayList.get(position);
+
+                String name = get_course_data.get("name");
+                String Class = get_course_data.get("Class");
+                String section = get_course_data.get("section");
+                String no_of_students = get_course_data.get("no_of_students");
+                String course_name = get_course_data.get("course_name");
+
+
+                holder.class_no_and_section_name.setText("Class : "+Class+" | Section : "+section);
+                holder.no_of_students.setText(no_of_students+" Students");
+                holder.course_name.setText("Course : "+course_name);
+
         }
 
         @Override
         public int getItemCount() {
-            return 10;
+            return arrayList.size();
         }
 
     }
